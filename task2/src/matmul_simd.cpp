b71a88ba3ebcfa0961f4e3cce45c0a7c8a7866df
Mohammed Nazal <mohammednazalm@gmail.com>
2026-09-05 06:20:08 +0000

    Task 2B : Added 256bit SIMD implementation

--- task2/src/matmul_simd.cpp
+++ task2/src/matmul_simd.cpp
@@ -6,21 +6,24 @@
 void matmul_simd(const float* A, const float* B, float* C,
                  int M, int N, int K, int lda, int ldb, int ldc) {
     // TODO(student): replace this placeholder with your register-tiled AVX2 implementation.
-    const int simd_width = 4;
-    float answer[simd_width];
+    const int simd_width = 8;
+    float answer[simd_width],sum=0;
     for(int i=0;i<M ;i++){
         for(int j=0;j<N;j++){
-            __m128 acc = _mm_setzero_ps();
+            sum=0;
+            __m256 acc =  _mm256_setzero_ps();
             const float* a = A + static_cast<long>(i) * lda;
             const float* b = B + static_cast<long>(j) * ldb;
             
             for (int p = 0; p < K; p+=simd_width) {
-                __m128 input_a = _mm_loadu_ps(a + p);
-                __m128 input_b = _mm_loadu_ps(b + p);
-                acc= _mm_fmadd_ps(input_a,input_b,acc);
+                __m256 input_a =  _mm256_loadu_ps(a + p);
+                __m256 input_b =  _mm256_loadu_ps(b + p);
+                acc=  _mm256_fmadd_ps(input_a,input_b,acc);
             }
-            _mm_storeu_ps(answer,acc);
-           int sum=answer[0]+answer[1]+answer[2]+answer[3];
+             _mm256_storeu_ps(answer,acc);
+           for(int k=0;k<simd_width;k++){
+            sum+=answer[k];
+           }
            C[static_cast<long>(i) * ldc + j] = sum;
         }
     }
@@ -32,20 +35,23 @@ void matmul_simd(const float* A, const float* B, float* C,
 //                  int M, int N, int K, int lda, int ldb, int ldc) {
 //     // TODO(student): replace this placeholder with your register-tiled AVX2 implementation.
 //     const int simd_width = 4;
-//     float answer[simd_width];
+//     float answer[simd_width],sum=0;
 //     for(int i=0;i<M ;i++){
 //         for(int j=0;j<N;j++){
-//             __m128 acc = _mm_setzero_ps();
+//              sum=0;  
+//             __m256 acc =  _mm256_setzero_ps();
 //             const float* a = A + static_cast<long>(i) * lda;
 //             const float* b = B + static_cast<long>(j) * ldb;
             
 //             for (int p = 0; p < K; p+=simd_width) {
-//                 __m128 input_a = _mm_loadu_ps(a + p);
-//                 __m128 input_b = _mm_loadu_ps(b + p);
-//                 acc= _mm_fmadd_ps(input_a,input_b,acc);
+//                 __m256 input_a =  _mm256_loadu_ps(a + p);
+//                 __m256 input_b =  _mm256_loadu_ps(b + p);
+//                 acc=  _mm256_fmadd_ps(input_a,input_b,acc);
 //             }
-//             _mm_storeu_ps(answer,acc);
-//            int sum=answer[0]+answer[1]+answer[2]+answer[3];
+//              _mm256_storeu_ps(answer,acc);
+//            for(int k=0;k<simd_width;k++){
+        //     sum+=answer[k];
+        //    }
 //            C[static_cast<long>(i) * ldc + j] = sum;
 //         }
 //     }

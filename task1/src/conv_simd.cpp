82caea2ea5c296054fc9094698dbef89fe41a049
Mohammed Nazal <70329337+mnazal@users.noreply.github.com>
2026-09-07 16:09:54 +0530

    Minor Fixes
    Implemented AVX2 multiplication and accumulation in conv_simd function.

--- task1/src/conv_simd.cpp
+++ task1/src/conv_simd.cpp
@@ -6,7 +6,6 @@
 
 void conv_simd(const float* in, float* out, const float* ker,
                int H, int W, int K) {
-    // TODO(student): replace this placeholder with your AVX2 implementation.
     const int p = K / 2;
     const int in_stride = W + 2 * p;
     const int simd_stride = 8;
@@ -18,8 +17,6 @@ void conv_simd(const float* in, float* out, const float* ker,
                     float weight= ker[ky*K+kx];
                     __m256 input = _mm256_loadu_ps(in + (oy+ky)*in_stride + (ox+kx));
                     __m256 kernel_input = _mm256_set1_ps(weight);
-                    // __m256 mul_output = _mm256_mul_ps(input, kernel_input);
-                    // acc = _mm256_add_ps(acc, mul_output);
                     acc = _mm256_fmadd_ps(input,kernel_input, acc);
                 }
             }
